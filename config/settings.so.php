7a2f9837f3f25dfb2fc60451090692bdd98edda1
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-12 10:38:04 +0300

    Файл конфигурации настройки модуля

--- config/settings.so.php
+++
@@ -1,1 +0,0 @@
-<?php return 'a:6:{s:10:"auditWrite";b:1;s:12:"templateMail";s:7:"default";s:16:"templateUserMail";s:7:"default";s:4:"mail";s:15:"admin@gmail.com";s:8:"sendMail";b:1;s:7:"captcha";b:1;}' ?>
